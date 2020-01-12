e7745012d1dbe98f922258d63ccaeec3118330b1
Lucas Bastos <lucasbastos@urpay.com.br>
2020-01-12 20:37:21 -0300

    adicionando conversão

--- lib/main.dart
+++ lib/main.dart
@@ -26,10 +26,23 @@ class _HomeState extends State<Home> {
   final realController = TextEditingController();
   final dolarController = TextEditingController();
   final euroController = TextEditingController();
+  void _realChanged(String text) {
+    double real = double.parse(text);
+    dolarController.text = (real / dolar).toStringAsFixed(2);
+    euroController.text = (real / euro).toStringAsFixed(2);
+  }
 
-  void _realChanged(String text) {}
-  void _dolarChanged(String text) {}
-  void _euroChanged(String text) {}
+  void _dolarChanged(String text) {
+    double dolar = double.parse(text);
+    realController.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
+  }
+
+  void _euroChanged(String text) {
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -99,6 +112,7 @@ Widget buildTextField(
         border: OutlineInputBorder(),
         prefixText: prefix),
     style: TextStyle(color: Colors.amber, fontSize: 25),
+    controller: controller,
     onChanged: f,
     keyboardType: TextInputType.number,
   );
